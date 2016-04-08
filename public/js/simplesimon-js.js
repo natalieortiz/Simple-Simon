6cd5dcb39b1f3db49b6e6394e6c1655e26c42567
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-08 15:16:07 -0500

    Updated javascript and jquery for Simple Simon.

--- public/js/simplesimon-js.js
+++ public/js/simplesimon-js.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 	var lights = [];
 	var	random;
 	var position = 0;
+	var counter = 0;
 	var delay = 2000;
 
 	$('#go').click(function (){
@@ -21,16 +22,17 @@ $(document).ready(function() {
 		console.log(lights);
 	}
 
+	function tallyScore(){
+		$('#scoreKeeper').html(counter);
+	}
+
 	//Flashes buttons in the array.
 	function buttonFlasher (){
 		lights.forEach(function(element,index){
-			var delay = 1000;
+			var delay = 600;
 			var flash = setTimeout(function(){	
-				$("#" + lights[index]).animate({
-					opacity: .25
-				},500).animate({
-					opacity: 1
-				},500);
+				$("#" + lights[index]).addClass("highlighted",delay);
+				$("#" + lights[index]).removeClass("highlighted",delay);
 			},delay*index);	
 		})
 
@@ -46,7 +48,8 @@ $(document).ready(function() {
 
 	//Stops the game
 	$('#stop').click(function (){
-		position = 0;
+		counter = 0;
+		lights = [];
 	});
 
 	//Compares user clicks with lights array.
@@ -57,14 +60,18 @@ $(document).ready(function() {
 			console.log(position);
 			position++;             
 		}   else {
-			alert("wrong");
+			alert("Game Over!");
 			position = 0;
 			console.log(position);
 		}
 		if (position == lights.length) { 
 			position = 0;
-			simonMove();
-			buttonFlasher();
+			var play = setTimeout(function(){
+				simonMove();
+				buttonFlasher();
+				counter++;
+				tallyScore(counter);
+			},1500);
 		}
 	});
 
