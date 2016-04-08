4adc5aeb5d4a99dbf981bfc1f7641f78ae6c7bc4
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-07 22:03:47 -0500

    Updated javascript.  Boxes can now flash in a sequence.

--- public/js/simplesimon-js.js
+++ public/js/simplesimon-js.js
@@ -4,54 +4,75 @@
 $(document).ready(function() {
 
 	var div = ["pink","green","orange","blue"];
-	console.log(div);
+	var lights = [];
+	var	random;
+	var position = 0;
+	var delay = 2000;
 
 	$('#go').click(function (){
-		lightUp();
+		simonMove();
+		buttonFlasher();
 	});
 
+	//Simon's turn in the game.
+	function simonMove(){
 
-	function lightUp(){
-
-		var random = div[Math.floor(Math.random()* 4)];
-		var play = [];
-		var user = 0;
-
-		$("#" + random).animate({
-			opacity: .25
-		},1000).animate({
-			opacity: 1
-		},1000);
-
-		$("#" + random).click(function(){
-			alert("you clicked the button");
-		});
+		randomize();
+		lights.push(random)
+		console.log(lights);
+		buttonFlasher();
+	}
+	//Flashes buttons in the array.
+	function buttonFlasher (){
+		lights.forEach(function(element,index){
+			var delay = 1000;
+			var flash = setTimeout(function(){	
+				$("#" + lights[index]).animate({
+					opacity: .25
+				},500).animate({
+					opacity: 1
+				},500);
+			},delay*index);	
+		})
 
 	}
 
-	//Konami exercise:
+	//Generates random number and chooses div.
+	function randomize(){
+		random = div[Math.floor(Math.random()* 4)];
+		console.log(random);
+	}
+
+	var pink = $("#pink");
+	var green = $("#green");
+	var orange = $("#orange");
+	var blue = $("#blue");
 	
-		// var konami = [38,38,40,40,37,39,37,39,66,65,13];
-		// var position = 0;
-		// console.log(konami.length);
 
-		// $(document).keyup(function(event){
-		// 	if (event.keyCode == konami[position]) {
-		// 		console.log("correct");
-		// 		console.log(event.keyCode);
-		// 		console.log(position);
-		// 		position++;              
-		// 	}   else {
-		// 		console.log("incorrect");
-		// 		position = 0;
-		// 	}
+	//Stops the game
+	$('#stop').click(function (){
+		clearInterval(timer);
+		position = 0;
+	});
 
-		// 	if (position == konami.length) { 
-		// 		alert("you win");
-		// 	}
+	//$(document).keyup(function(event){
+	// 	if (event.keyCode == konami[position]) {
+	// 		position++;              
+	// 	}   else {
+	// 		console.log("incorrect");
+	// 		position = 0;
+	// 	}
 
-		// });
+	// 	if (position == konami.length) { 
+		// alert("you win");
+	// 	}
 
+	// });
+
+	// var timeoutId = setTimeout(function () {
+	//     clearInterval(timer);
+	//     alert('You lose!');
+	// }, 10000);
 
 });
 })();
